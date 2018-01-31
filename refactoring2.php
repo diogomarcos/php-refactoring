b98e340f76f2d44816126c44a2b90590da267cb7
Diogo Marcos <contato@diogomarcos.com>
2018-01-31 07:45:56 -0200

    Adding file with original code: refactoring2

---
+++ refactoring2.php
@@ -0,0 +1,14 @@
+<?php
+
+class MyUserClass
+{
+    public function getUserList()
+    {
+        $dbconn = new DatabaseConnection('localhost', 'user', 'password');
+        $results = $dbconn->query('select name from user');
+
+        sort($results);
+
+        return $results;
+    }
+}
